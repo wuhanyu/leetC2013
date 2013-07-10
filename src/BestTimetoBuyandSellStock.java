80491024c2f7fa5ebe62018e3b46ec8100a4e9af
SimonWu <wuhanyu@gmail.com>
2013-07-10 20:59:06 +0800

    update the  divide and conquer

--- src/BestTimetoBuyandSellStock.java
+++ src/BestTimetoBuyandSellStock.java
@@ -13,39 +13,22 @@ public class BestTimetoBuyandSellStock {
 
 	public int maxProfit(int[] prices) {
 		//Extreme input
-		if (prices.length == 0) return 0;
-		int[][] sums = new int[prices.length][prices.length];
-		int[] vals = new int[prices.length];
-		for (int i = 1; i < vals.length; i++){
-			vals[i] = prices[i] - prices[i - 1];
-			sums[i][i] = vals[i];
-		}
-//		System.out.println(Arrays.toString(vals));
+		if (prices == null || prices.length <= 1) return 0;
 		
-		for (int i = 0; i < vals.length; i++){
-			for (int j = i + 1; j < vals.length; j++){
-				sums[i][j] = sums[i][j-1] + vals[j];
-			}
-		}
-		return maxP(vals, sums, 0, vals.length - 1);	
+		return maxP(prices, 0,prices.length - 1);	
 	}
 
-	int maxP(int[] vals, int[][] sums, int start, int end) {
-		if (start == end) return vals[start];
-		int max = 0;
+	int maxP(int[] prices, int start, int end) {
+		if (start == end) return 0;
 		int mid = (start + end) / 2;
-		int maxsum = 0;
-		for (int i = 0; i <= mid; i++){
-			if (sums[i][mid] > maxsum) maxsum = sums[i][mid];
-		}
-		max += maxsum;
-		maxsum = 0;
-		for (int i = mid + 1; i <= end; i++){
-			if (sums[mid + 1][i] > maxsum) maxsum = sums[mid + 1][i];
-		}
-		max += maxsum;
-		max = Math.max(max, maxP(vals, sums, start, mid));
-		max = Math.max(max, maxP(vals, sums, mid + 1, end));
+		int max = 0;
+		int maxval = Integer.MIN_VALUE;
+		int minval = Integer.MAX_VALUE;
+		for (int i = start; i <= mid; i++) minval = Math.min(minval, prices[i]);
+		for (int i = mid + 1; i <= end; i++) maxval = Math.max(maxval, prices[i]);
+		max = maxval - minval;
+		max = Math.max(max, maxP(prices, start, mid));
+		max = Math.max(max, maxP(prices, mid + 1, end));
 		return max;
 	}
 
