fa962b5f8c1b1c37c6a72486693101d0c3a4cbcf
SimonWu <wuhanyu@gmail.com>
2013-07-12 19:03:37 +0800

    21

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -1,6 +1,21 @@
 import java.util.ArrayList;
 
+/**
+ * @author Simon@itechs
+ Given numRows, generate the first numRows of Pascal's triangle.
 
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+ *
+ */
 public class PascalsTriangle {
 
 	/**
@@ -10,29 +25,30 @@ public class PascalsTriangle {
 		// TODO Auto-generated method stub
 		System.out.println(new PascalsTriangle().generate(3));
 	}
-	
-    public ArrayList<ArrayList<Integer>> generate(int numRows) {
-    	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        if (numRows == 0) return result;
-        ArrayList<Integer> init = new ArrayList<Integer>();
-        init.add(1);
-        result.add(init);
-        for (int i = 2; i <= numRows; i++){
-        	ArrayList<Integer> newline = getNewLine(init);
-        	result.add(newline);
-        	init = newline;
-        }
-        return result;
-    }
-    
-    ArrayList<Integer> getNewLine(ArrayList<Integer> input){
-    	ArrayList<Integer> result = new ArrayList<Integer>();
-    	result.add(1);
-    	for (int i = 0; i < input.size() - 1; i++){
-    		result.add(input.get(i) + input.get(i + 1));
-    	}
-    	result.add(1);
-    	return result;
-    }
+
+	public ArrayList<ArrayList<Integer>> generate(int numRows) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		if (numRows == 0)
+			return result;
+		ArrayList<Integer> init = new ArrayList<Integer>();
+		init.add(1);
+		result.add(init);
+		for (int i = 2; i <= numRows; i++) {
+			ArrayList<Integer> newline = getNewLine(init);
+			result.add(newline);
+			init = newline;
+		}
+		return result;
+	}
+
+	ArrayList<Integer> getNewLine(ArrayList<Integer> input) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		result.add(1);
+		for (int i = 0; i < input.size() - 1; i++) {
+			result.add(input.get(i) + input.get(i + 1));
+		}
+		result.add(1);
+		return result;
+	}
 
 }
--- src/PascalsTriangleII.java
+++ src/PascalsTriangleII.java
@@ -2,6 +2,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 
+/**
+ * @author Simon@itechs
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+ */
 public class PascalsTriangleII {
 
 	/**
