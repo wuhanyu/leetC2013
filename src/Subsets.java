6869a4c9769a7ceb9f5eea56360e5d0e336a53a2
Simon Wu <wuhanyu@gmail.com>
2013-07-24 18:57:21 +0800

    Create Subsets.java

---
+++ src/Subsets.java
@@ -0,0 +1,18 @@
+public class Subsets {
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        Arrays.sort(S);
+        traversal(0, S, new ArrayList<Integer>(), result);
+        return result;
+    }
+    
+    void traversal(int n, int[]S, ArrayList<Integer> list, ArrayList<ArrayList<Integer>> result){
+        if (n == S.length) result.add((ArrayList<Integer>) list.clone());
+        else {
+            traversal(n + 1, S, list, result);
+            list.add(S[n]);
+            traversal(n + 1, S, list, result);
+            list.remove(list.size() - 1);
+        }
+    }
+}
