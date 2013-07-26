f6dfb94fb5f40937b4fb94a443bead3ba3cbe0a3
Simon Wu <wuhanyu@gmail.com>
2013-07-26 21:52:58 +0800

    Create GenerateParentheses.java

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,15 @@
+public class GenerateParentheses {
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> result = new ArrayList<String>();
+        getResult(0, 0, n, result, "");
+        return result;
+    }
+    void getResult(int leftnum, int rightnum, int n, ArrayList<String> result, String s){
+        if (leftnum == n && rightnum == n) {
+            result.add(s);
+        } else {
+            if (leftnum < n) getResult(leftnum + 1, rightnum, n, result, s + "(");
+            if (rightnum < leftnum) getResult(leftnum, rightnum + 1,  n, result, s + ")");
+        }
+    }
+}
