db89b969cfaf10d555272963250516238a047612
Simon Wu <wuhanyu@gmail.com>
2013-08-06 00:12:15 +0800

    Update SetMatrixZeroes.java

--- src/SetMatrixZeroes.java
+++ src/SetMatrixZeroes.java
@@ -4,22 +4,33 @@ public class SetMatrixZeroes {
         if (m == 0) return;
         int n = matrix[0].length;
         if (n == 0) return;
-        boolean[] x = new boolean[m];
-        boolean[] y = new boolean[n];
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
+        boolean x = false;
+        boolean y = false;
+        for (int i = 0; i < m; i++)
+            if (matrix[i][0] == 0) x = true;
+        for (int i = 0; i < n; i++)
+            if (matrix[0][i] == 0) y = true;
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
                 if (matrix[i][j] == 0){
-                    x[i] = true;
-                    y[j] = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (x[i] || y[j]){
+        
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                if (matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
+        }
+        if (x){
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;
+        }
+        if (y){
+            for (int i = 0; i < n; i++) matrix[0][i] = 0;
         }
     }
 }
