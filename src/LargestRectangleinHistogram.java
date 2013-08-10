ee2f6d336b1bdc94748b37e698c0fce1768372e7
SimonWu <wuhanyu@gmail.com>
2013-08-10 19:48:31 +0800

    update LargestRectangleinHistogram O(n^2) to O(n) implemented by Stack

--- src/LargestRectangleinHistogram.java
+++ src/LargestRectangleinHistogram.java
@@ -1,16 +1,27 @@
+import java.util.Stack;
+
 public class LargestRectangleinHistogram {
+	public static void main(String[] args){
+		int[] height = {2, 1, 2};
+		System.out.println(new LargestRectangleinHistogram().largestRectangleArea(height));
+	}
+	
     public int largestRectangleArea(int[] height) {
         if (height.length == 0) return 0;
-        int maxarea = 0;
-        for (int i = 0; i < height.length; i++){
-            if (i > 0 && height[i] == height[i-1]) continue;
-            int s = i;
-            while (s > 0 && height[s-1] >= height[i]) s--;
-            int t = i;
-            while (t < height.length - 1 && height[t+1] >= height[i]) t++;
-            int area = (t - s + 1) * height[i];
-            if (area > maxarea) maxarea = area;
+        Stack<Integer> indexS = new Stack<Integer>();
+        int max = 0;
+        for (int i = 0; i <= height.length; i++){
+            if (i < height.length && (indexS.isEmpty() || height[indexS.peek()] <= height[i])){
+                indexS.push(i);
+            } else {
+                int tmp = indexS.pop();
+                int width;
+                if (indexS.isEmpty()) width = i;
+                else width = i - indexS.peek() - 1;
+                max = Math.max(max, height[tmp] * width);
+                if (i < height.length || !indexS.isEmpty()) i--;
+            }
         }
-        return maxarea;
+        return max;
     }
 }
