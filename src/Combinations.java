7c41196f231b05ea90c932399ed9c92e5b2a6ad1
SimonWu <wuhanyu@gmail.com>
2013-08-17 20:53:45 +0800

    add Combinations

--- src/Combinations.java
+++ src/Combinations.java
@@ -1,28 +1,34 @@
 import java.util.ArrayList;
 
 public class Combinations {
-    static ArrayList<ArrayList<Integer>> result;
-    static boolean[] flag;
+	
+	public static void main(String[] args){
+		System.out.println(new Combinations().combine(3, 2));
+	}
+	
     public ArrayList<ArrayList<Integer>> combine(int n, int k) {
-        result = new ArrayList<ArrayList<Integer>>();
-        if (n < k) return result;
-        flag = new boolean[n];
-        traversal(1, k, n, 0, new ArrayList<Integer>());
-        return result;
-    }
-    
-    void traversal(int p, int k, int n, int start, ArrayList<Integer> list){
-        if (p > k) result.add((ArrayList<Integer>) list.clone());
-        else {
-            for (int i = start; i < n; i++){
-                if (!flag[i]){
-                    list.add(i + 1);
-                    flag[i] = true;
-                    traversal(p + 1, k, n, i+1, list);
-                    list.remove(list.size() - 1);
-                    flag[i] = false;
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (n <= 0) return result;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < k; i++) list.add(i+1);
+        int p = k;
+        while (p >= 0){
+            if (p == k) {
+                result.add((ArrayList<Integer>) list.clone());
+                p--;
+            } else {
+                int cur = list.get(p);
+                if (cur == 0) cur = list.get(p-1) + 1;
+                else cur++;
+                if (cur == n + 1){          
+                    list.set(p, 0);
+                    p--;
+                } else {
+                	list.set(p, cur);
+                    p++;
                 }
             }
         }
+        return result;
     }
 }
