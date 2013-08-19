5aab81c891793df9bf22ac64fe7a3717198b89b3
Simon Wu <wuhanyu@gmail.com>
2013-08-20 00:21:32 +0800

    Update PathSum.java

--- src/PathSum.java
+++ src/PathSum.java
@@ -1,39 +1,18 @@
 /**
- * 
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
-
-/**
- * @author Simon@itechs
- * @see http://leetcode.com/onlinejudge#question_112
- * @problem Given a binary tree and a sum, determine if the tree has a
- *          root-to-leaf path such that adding up all the values along the path
- *          equals the given sum.
- * 
- *          For example: Given the below binary tree and sum = 22, 5 / \ 4 8 / /
- *          \ 11 13 4 / \ \ 7 2 1 return true, as there exist a root-to-leaf
- *          path 5->4->11->2 which sum is 22.
- */
-public class PathSum {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	
-	
+public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
-        if (root.left == null && root.right == null){
-        	if (root.val == sum) return true;
-        } else {
-        	if (hasPathSum(root.left, sum - root.val)) return true;
-        	if (hasPathSum(root.right, sum - root.val)) return true;
-        }
-        return false;
+        if (root.left == null && root.right == null && root.val == sum) return true;
+        sum -= root.val;
+        if (hasPathSum(root.left, sum)) return true;
+        return hasPathSum(root.right, sum);
     }
-
 }
