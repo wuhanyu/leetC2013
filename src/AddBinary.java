71b8160140879a063125a6f999042fb6edc57c3f
Simon Wu <wuhanyu@gmail.com>
2013-09-05 16:48:04 +0800

    Update AddBinary.java

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -1,47 +1,22 @@
-public class AddBinary {
+public class Solution {
     public String addBinary(String a, String b) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int n = Math.max(a.length(), b.length()) + 1;
-        if (n == 1) return "";
-        char[] chs = new char[n];
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int p = n - 1;
+        int maxlen = Math.max(a.length(), b.length());
+        String ar = reverse(a);
+        String br = reverse(b);
+        String result = "";
         int plus = 0;
-        while (i >= 0 && j >= 0){
-            int ai = a.charAt(i) - '0';
-            int bi = b.charAt(j) - '0';
-            int ri = (ai + bi + plus) % 2;
-            plus = (ai + bi + plus) / 2;
-            if (ri == 0) chs[p] = '0';
-            else chs[p] = '1';
-            i--;
-            j--;
-            p--;
+        for (int i = 0; i < maxlen; i++){
+            int ai = (i < ar.length()) ? ar.charAt(i) - '0' : 0;
+            int bi = (i < br.length()) ? br.charAt(i) - '0' : 0;
+            int val = (ai + bi + plus) & 1;
+            plus = (ai + bi + plus) >> 1;
+            result += (char)('0' + val);
         }
-        while (i >= 0){
-            int ai = a.charAt(i) - '0';
-            int ri = (ai + plus) % 2;
-            plus = (ai + plus) / 2;
-            if (ri == 0) chs[p] = '0';
-            else chs[p] = '1';
-            i--;
-            p--;
-        }
-        while (j >= 0){
-            int bi = b.charAt(j) - '0';
-            int ri = (bi + plus) % 2;
-            plus = (bi + plus) / 2;
-            if (ri == 0) chs[p] = '0';
-            else chs[p] = '1';
-            j--;
-            p--;
-        }
-        if (plus > 0) chs[0] = '1';
-        else chs[0] = '0';
-        String result = String.valueOf(chs);
-        if (plus == 0) result = result.substring(1, result.length());
-        return result;
+        if (plus > 0) result += '1';
+        return reverse(result);
+    }
+    
+    String reverse(String input){
+        return new StringBuilder(input).reverse().toString();
     }
 }
