a8a67e5220864ce57e0866fcb77bd8b896a37aea
Simon Wu <wuhanyu@gmail.com>
2013-09-05 19:45:14 +0800

    Update ConvertSortedArraytoBinarySearchTree.java

--- src/ConvertSortedArraytoBinarySearchTree.java
+++ src/ConvertSortedArraytoBinarySearchTree.java
@@ -1,36 +1,27 @@
 /**
- * 
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
-
-/**
- * @author Simon@itechs
- * @see http://leetcode.com/onlinejudge#question_108
- * @problem Given an array where elements are sorted in ascending order, convert
- *          it to a height balanced BST.
- */
-public class ConvertSortedArraytoBinarySearchTree {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
+public class Solution {
+    int pi;
     public TreeNode sortedArrayToBST(int[] num) {
-        if (num.length == 0) return null;
-        TreeNode root = constructTree(num, 0, num.length - 1);
-        return root;
+        pi = 0;
+        return buildTree(num, 0, num.length - 1);
     }
     
-    TreeNode constructTree(int[] num, int start, int end){
-    	if (start > end) return null;
-    	int m = (start + end) / 2;
-    	TreeNode root = new TreeNode(num[m]);
-    	root.left = constructTree(num, start, m - 1);
-    	root.right = constructTree(num, m + 1, end);
-    	return root;
+    TreeNode buildTree(int[] num, int start, int end){
+        if (start > end) return null;
+        int mid = start + (end - start) / 2;
+        TreeNode left = buildTree(num, start, mid - 1);
+        TreeNode root = new TreeNode(num[pi]);
+        pi++;
+        root.left = left;
+        root.right = buildTree(num, mid + 1, end);
+        return root;
     }
-
 }
