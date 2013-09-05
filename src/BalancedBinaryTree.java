f0083186134a873f461ea4e8ae06f36d22e7a4bc
Simon Wu <wuhanyu@gmail.com>
2013-09-05 19:17:38 +0800

    Update BalancedBinaryTree.java

--- src/BalancedBinaryTree.java
+++ src/BalancedBinaryTree.java
@@ -1,39 +1,25 @@
 /**
- * 
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
-
-/**
- * @author Simon
- * @see http://leetcode.com/onlinejudge#question_110
- * @problem Given a binary tree, determine if it is height-balanced.
- * 
- *          For this problem, a height-balanced binary tree is defined as a
- *          binary tree in which the depth of the two subtrees of every node
- *          never differ by more than 1.
- */
-public class BalancedBinaryTree {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
+public class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        if (isBalanced(root.left) && isBalanced(root.right)){
-        	int left = 0;
-        	int right = 0;
-        	if (root.left != null) left = root.left.val;
-        	if (root.right != null) right = root.right.val;
-        	root.val = Math.max(left, right) + 1;
-        	if (Math.abs(left - right) <= 1){
-        		return true;
-        	}
-        }
-        return false;
+        if (getHeight(root) < 0) return false;
+        return true;
     }
-
+    
+    public int getHeight(TreeNode root){
+        if (root == null) return 0;
+        int leftHeight = getHeight(root.left);
+        if (leftHeight < 0) return -1;
+        int rightHeight = getHeight(root.right);
+        if (rightHeight < 0) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
