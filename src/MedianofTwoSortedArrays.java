5dbaa85688948745ce0a6d95983e7eba9c204cd4
Simon Wu <wuhanyu@gmail.com>
2013-09-16 20:29:45 +0800

    Update MedianofTwoSortedArrays.java

--- src/MedianofTwoSortedArrays.java
+++ src/MedianofTwoSortedArrays.java
@@ -1,75 +1,28 @@
-import java.util.Arrays;
-
-/**
- * @author Simon
- * @see http://leetcode.com/onlinejudge#question_2
- * @problem There are two sorted arrays A and B of size m and n respectively.
- *          Find the median of the two sorted arrays. The overall run time
- *          complexity should be O(log (m+n)).
- */
-public class MedianofTwoSortedArrays {
-	public static void main(String[] args) {
-		int[] A = {1, 1};
-		int[] B = {1, 2};
-		System.out.println(new MedianofTwoSortedArrays()
-				.findMedianSortedArrays(A, B));
-	}
-
-	public double findMedianSortedArrays(int A[], int B[]) {
-		int n = A.length + B.length;
-		if (n == 0) return 0;
-		int ai, bi;
-		ai = 0;
-		bi = 0;
-		double median = 0;
-		if (n % 2 == 1){
-			for (int i = 0; i < n / 2 + 1; i++){
-				if (ai == A.length){
-					median = B[bi];
-					bi++;
-				} else if (bi == B.length){
-					median = A[ai];
-					ai++;
-				} else if (A[ai] <  B[bi]){
-					median = A[ai];
-					ai++;
-				} else {
-					median = B[bi];
-					bi++;
-				}
-			}
-		} else {
-			int lower = 0, upper;
-			for (int i = 0; i < n / 2; i++){
-				if (ai == A.length){
-					lower = B[bi];
-					bi++;
-				} else if (bi == B.length){
-					lower = A[ai];
-					ai++;
-				} else if (A[ai] <  B[bi]){
-					lower = A[ai];
-					ai++;
-				} else {
-					lower = B[bi];
-					bi++;
-				}				
-			}
-			if (ai == A.length){
-				upper = B[bi];
-				bi++;
-			} else if (bi == B.length){
-				upper = A[ai];
-				ai++;
-			} else if (A[ai] <  B[bi]){
-				upper = A[ai];
-				ai++;
-			} else {
-				upper = B[bi];
-				bi++;
-			}
-			median = (double)(upper + lower) / 2;
-		}
-		return median;
-	}
+/*
+* nlog(n)
+*/
+public class Solution {
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int m = A.length;
+        int n = B.length;
+        if ((m + n) % 2 == 0)
+            return (double)(getKthNumber(A, 0, m, B, 0, n, (m + n) / 2) + getKthNumber(A, 0, m, B, 0, n, (m + n) / 2 + 1)) / 2;
+        else
+            return (double)getKthNumber(A, 0, m, B, 0, n, (m + n) / 2 + 1);
+    }
+    
+    int getKthNumber(int[] A, int as, int ae, int[] B, int bs, int be, int k){
+        if (as >= ae) return B[bs + k - 1];
+        if (bs >= be) return A[as + k - 1];
+        if (k <= 1) return Math.min(A[as], B[bs]);
+        int am = as + (ae - as) / 2;
+        int bm = bs + (be - bs) / 2;
+        if (A[am] <= B[bm]){
+            if (am - as + bm - bs + 1 >= k) return getKthNumber(A, as, ae, B, bs, bm, k);
+            else return getKthNumber(A, am + 1, ae, B, bs, be, k - am + as - 1);
+        } else {
+            if (am - as + bm - bs + 1 >= k) return getKthNumber(A, as, am, B, bs, be, k);
+            else return getKthNumber(A, as, ae, B, bm + 1, be, k - bm + bs - 1);
+        }
+    }
 }
