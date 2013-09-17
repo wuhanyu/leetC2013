bb504e8e3354d06f752def29ab0f83cc574d6e56
Simon Wu <wuhanyu@gmail.com>
2013-09-17 12:41:46 +0800

    Update MergeSortedArray.java

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,27 +1,17 @@
-public class MergeSortedArray {
+public class Solution {
     public void merge(int A[], int m, int B[], int n) {
-         int p = A.length - 1;
-         m = m - 1;
-         n = n - 1;
-         while (m >= 0 && n >= 0){
-             if (A[m] > B[n]){
-                 A[p] = A[m];
-                 m--;
-             } else {
-                 A[p] = B[n];
-                 n--;
-             }
-             p--;
-         }
-         while (m >= 0){
-             A[p] = A[m];
-             m--;
-             p--;
-         }
-         while (n >= 0){
-             A[p] = B[n];
-             n--;
-             p--;
-         }
+        int bi = B.length - 1;
+        int ai = A.length - B.length - 1;
+        for (int i = A.length - 1; i >= 0; i--){
+            int a = ai >= 0 ? A[ai] : Integer.MIN_VALUE;
+            int b = bi >= 0 ? B[bi] : Integer.MIN_VALUE;
+            if (a > b) {
+                A[i] = a;
+                ai--;
+            } else {
+                A[i] = b;
+                bi--;
+            }
+        }
     }
 }
