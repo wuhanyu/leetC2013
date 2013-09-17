0bb2d7f0ff6e73fd464ebddb33923864ac34bd62
Simon Wu <wuhanyu@gmail.com>
2013-09-17 11:31:36 +0800

    Update SortColors.java

--- src/SortColors.java
+++ src/SortColors.java
@@ -1,36 +1,25 @@
-public class SortColors {
+public class Solution {
     public void sortColors(int[] A) {
+        int red = 0;
+        int blue = A.length - 1;
         int i = 0;
-        int j = A.length - 1;
-        while (i <= j && A[i] == 0) i++;
-        while (i <= j && A[j] == 2) j--;
-        int p = i;
-        int k = j;
-        while (p <= k){
-            if (A[p] == 2){
-                A[p] = A[j];
-                A[j] = 2;
-                j--;
-                if (j < k) k = j;
-            } else if (A[p] == 0){
-                A[p] = A[i];
-                A[i] = 0;
-                p++;
+        while (i <= blue){
+            if (A[i] == 0){
+                swap(A, i, red);
+                red++;
                 i++;
-            } else if (A[k] == 0){
-                A[k] = A[i];
-                A[i] = 0;
-                i++;
-                if (i > p) p = i;
-            } else if (A[k] == 2){
-                A[k] = A[j];
-                A[j] = 2;
-                k--;
-                j--;
+            } else if (A[i] == 2) {
+                swap(A, i, blue);
+                blue--;
             } else {
-                p++;
-                k--;
+                i++;
             }
         }
+    }
+    
+    void swap(int[] A, int i, int j){
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
     }
 }
