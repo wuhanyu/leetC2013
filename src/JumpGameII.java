f49a3eb9637925a29c1441ddb82f134d3fe203eb
Simon Wu <wuhanyu@gmail.com>
2013-09-27 11:02:39 +0800

    Update JumpGameII.java

--- src/JumpGameII.java
+++ src/JumpGameII.java
@@ -1,26 +1,23 @@
 public class JumpGameII {
     public int jump(int[] A) {
         if (A.length <= 1) return 0;
-        int[] jumps = new int[A.length + 1];
-        jumps[1] = A[0];
-        if (A[0] >= A.length - 1) return 1;
-        int maxjump = 1;
-        for (int i = 1; i < A.length; i++){
-            int j = 1;
-            while (j <= maxjump){
-                if (jumps[j] >= i){
-                    int target = i + A[i];
-                    if (target >= A.length - 1){
-                        return j + 1;
-                    } else if (i + A[i] > jumps[j+1]){
-                        jumps[j+1] = i + A[i];
-                        if (j + 1 > maxjump) maxjump = j + 1;
-                    }
-                    break;
-                }
-                j++;
+        int begin = 0;
+        int end = 0;
+        int count = 0;
+        while (true) {
+            int newend = 0;
+            for (int i = begin; i <= end; i++) {
+                if (A[i] + i > newend) newend = A[i] + i;
             }
+            if (newend > end) {
+                count++;
+                if (newend >= A.length - 1) return count;
+                else {
+                    begin = end + 1;
+                    end = newend;
+                }
+            } else break;
         }
-        return 0;
+        return -1;
     }
 }
