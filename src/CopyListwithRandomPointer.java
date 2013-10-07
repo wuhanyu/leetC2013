39bca6fbc9cf0fed2ba9effaf38a677a6e13c7b1
Simon Wu <wuhanyu@gmail.com>
2013-10-07 14:08:23 +0800

    Update CopyListwithRandomPointer.java

--- src/CopyListwithRandomPointer.java
+++ src/CopyListwithRandomPointer.java
@@ -9,20 +9,41 @@
 public class CopyListwithRandomPointer {
     public RandomListNode copyRandomList(RandomListNode head) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
-        HashMap<RandomListNode, RandomListNode> dict = new HashMap<RandomListNode, RandomListNode>();
-        return copyRandomList(head, dict);
-    }
-    
-    public RandomListNode copyRandomList(RandomListNode head, HashMap<RandomListNode, RandomListNode> dict) {
+        
+        //O(n) space, O(n) time
         if (head == null) return null;
-        if (dict.containsKey(head)) {
-            return dict.get(head);
-        } else {
-            RandomListNode newnode = new RandomListNode(head.label);
-            dict.put(head, newnode);
-            newnode.next = copyRandomList(head.next, dict);
-            newnode.random = copyRandomList(head.random, dict);
-            return newnode;
+        //create
+        RandomListNode nhead = new RandomListNode(head.label);
+        RandomListNode p = head;
+        RandomListNode np = nhead;
+        RandomListNode next;
+        ArrayList<RandomListNode> ori = new ArrayList<RandomListNode>();
+        while (p.next != null) {
+            ori.add(p);
+            np.next = new RandomListNode(p.next.label);
+            next = p.next;
+            p.next = np;
+            np.random = p;
+            p = next;
+            np = np.next;
+        } 
+        ori.add(p);
+        p.next = np;
+        np.random = p;
+        //create link
+        np = nhead;
+        while (np != null) {
+            if (np.random.random != null)
+                np.random = np.random.random.next;
+            else
+                np.random = null;
+            np = np.next;
         }
+        //restore
+        for (int i = 0; i < ori.size() - 1; i++){
+            ori.get(i).next = ori.get(i + 1);
+        }
+        ori.get(ori.size() - 1).next = null;
+        return nhead;
     }
 }
