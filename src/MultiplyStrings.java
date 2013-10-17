9c8d6e96072d169f486917c0edf76ea586d0e593
Simon Wu <wuhanyu@gmail.com>
2013-10-17 20:45:17 +0800

    Update MultiplyStrings.java

--- src/MultiplyStrings.java
+++ src/MultiplyStrings.java
@@ -11,61 +11,25 @@
  *          Note: The numbers can be arbitrarily large and are non-negative.
  */
 public class MultiplyStrings {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String num1 = "6913259244";
-		String num2 = "0";
-		System.out.println(new MultiplyStrings().multiply(num1, num2));
-	}
-
-    static int max;
     public String multiply(String num1, String num2) {
-        int[] result = new int[1000];
-        int[] n1 = reverse(num1.length(), num1.toCharArray());
-        int[] n2 = reverse(num2.length(), num2.toCharArray());
-               
-        max = n1.length;
-        for (int i = 0; i < n2.length; i++){
-            multi(i, result, n1, n2[i]);
-        }
-        
-        String rs = "";
-        boolean start = false;
-        for (int i = 0; i < max; i++){
-        	if (!start &&  result[max - i - 1] == 0) continue;
-        	start = true;
-            rs += (char)('0' + result[max - i - 1]);
-        }
-        if (rs.length() == 0) return "0";
-        return rs;
-    }
-    
-    void multi(int start, int[] result, int[] mul1, int mul2){
+        String nr1 = new StringBuilder(num1).reverse().toString();
+        String nr2 = new StringBuilder(num2).reverse().toString();
+        char[] result = new char[nr1.length() + nr2.length()];
+        for (int i = 0; i < result.length; i++) result[i] = '0';
         int plus = 0;
-        for (int i = 0; i < mul1.length; i++){
-            int t = mul1[i] * mul2 + result[start + i] + plus;
-            result[start + i] = t % 10;
-            plus = t / 10;
+        for (int i = 0; i < nr2.length(); i++) {
+            plus = 0;
+            for (int j = 0; j < nr1.length(); j++) {
+                int val = (nr1.charAt(j) - '0') * (nr2.charAt(i) - '0') + plus + result[i + j] - '0';
+                result[i + j] = (char)('0' + val % 10);
+                plus = val / 10;
+            }
+            if (plus > 0) result[i + nr1.length()] = (char)('0' + plus);
         }
-        if (plus == 0){
-        	if (max < start + mul1.length)
-        		max = start + mul1.length;
-        } else {
-        	if (max < start + mul1.length + 1){
-        		max = start + mul1.length + 1;
-        		result[max - 1] = plus;
-        	}
-        }
-    }
-    
-    int[] reverse(int n, char[] input){
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++){
-            result[i] = input[n-i-1] - '0';
-        }
-        return result;
+        String str = new String(result);
+        int i = str.length() - 1;
+        while (str.charAt(i) == '0' && i > 0) i--;
+        str = str.substring(0, i + 1);
+        return new StringBuilder(str).reverse().toString();
     }
 }
