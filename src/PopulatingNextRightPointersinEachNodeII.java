cb59496bc760cebe135c4e3606d16b6bcee25b62
Simon Wu <wuhanyu@gmail.com>
2013-10-17 21:11:46 +0800

    Update PopulatingNextRightPointersinEachNodeII.java

--- src/PopulatingNextRightPointersinEachNodeII.java
+++ src/PopulatingNextRightPointersinEachNodeII.java
@@ -1,66 +1,33 @@
-import java.util.ArrayList;
-
 /**
- * 
- */
-
-/**
- * @author Simon@itechs
- * @see http://leetcode.com/onlinejudge#question_117
- * @problem Follow up for problem "Populating Next Right Pointers in Each Node".
- * 
- *          What if the given tree could be any binary tree? Would your previous
- *          solution still work?
- * 
- *          Note:
- * 
- *          You may only use constant extra space. For example, Given the
- *          following binary tree,
- * 
- *          1 / \ 2 3 / \ \ 4 5 7 After calling your function, the tree should
- *          look like:
- * 
- *          1 -> NULL / \ 2 -> 3 -> NULL / \ \ 4-> 5 -> 7 -> NULL
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
  */
 public class PopulatingNextRightPointersinEachNodeII {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TreeLinkNode root = new TreeLinkNode(1);
-		TreeLinkNode left = new TreeLinkNode(2);
-		TreeLinkNode ll = new TreeLinkNode(3);
-		TreeLinkNode lr = new TreeLinkNode(3);
-		root.left = left;
-		left.left = ll;
-		left.right = lr;
-		new PopulatingNextRightPointersinEachNodeII().connect(root);
-	}
-
-	public void connect(TreeLinkNode root) {
-		if (root == null || (root.left == null && root.right == null))
-			return;
-		
-		TreeLinkNode t = null;
-		TreeLinkNode next = null;
-		if (root.left != null && root.right != null){
-			root.left.next = root.right;
-			t = root.right;
-		} else if (root.left != null){
-			t = root.left;
-		} else {
-			t = root.right;
-		}
-		next = root.next;
-		while (next != null && next.left == null && next.right == null) next = next.next;
-		if (next != null){
-			if (next.left != null) t.next = next.left;
-			else t.next = next.right;
-		}
-		connect(root.right);
-		connect(root.left);
-		
-	}
+    public void connect(TreeLinkNode root) {
+        if (root == null) return;
+        if (root.left != null && root.right != null) {
+            root.left.next = root.right;
+        }
+        TreeLinkNode cur = root.left;
+        if (root.right != null) cur = root.right;
+        if (cur != null) {
+            TreeLinkNode p = root.next;
+            while (p != null) {
+                if (p.left != null) {
+                    cur.next = p.left;
+                    break;
+                } else if (p.right != null) {
+                    cur.next = p.right;
+                    break;
+                }
+                p = p.next;
+            }
+        }
+        connect(root.right);
+        connect(root.left);
+    }
 }
