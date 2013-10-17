115ebdee128ec96a7bedb1d354a68c8c932a3e1a
Simon Wu <wuhanyu@gmail.com>
2013-10-17 21:22:42 +0800

    Update BinaryTreeLevelOrderTraversal.java

--- src/BinaryTreeLevelOrderTraversal.java
+++ src/BinaryTreeLevelOrderTraversal.java
@@ -1,44 +1,30 @@
-import java.util.ArrayList;
-
 /**
- * 
- */
-
-/**
- * @author Simon@itechs
- * @see http://leetcode.com/onlinejudge#question_102
- * @problem Given a binary tree, return the level order traversal of its nodes'
- *          values. (ie, from left to right, level by level).
- * 
- *          For example: Given binary tree {3,9,20,#,#,15,7},
- * 
- *          3 / \ 9 20 / \ 15 7 return its level order traversal as:
- * 
- *          [ [3], [9,20], [15,7] ]
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
 public class BinaryTreeLevelOrderTraversal {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		traversal(root, 1, result);
-		return result;
-	}
-
-	void traversal(TreeNode e, int level, ArrayList<ArrayList<Integer>> result) {
-		if (e == null)
-			return;
-		while (result.size() < level)
-			result.add(new ArrayList<Integer>());
-		result.get(level - 1).add(e.val);
-		traversal(e.left, level + 1, result);
-		traversal(e.right, level + 1, result);
-	}
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (root == null) return result;
+        ArrayList<TreeNode> cur = new ArrayList<TreeNode>();
+        cur.add(root);
+        while (cur.size() > 0) {
+            ArrayList<Integer> line = new ArrayList<Integer>();
+            ArrayList<TreeNode> children = new ArrayList<TreeNode>();
+            for (int i = 0; i < cur.size(); i++) {
+                TreeNode e = cur.get(i);
+                line.add(e.val);
+                if (e.left != null) children.add(e.left);
+                if (e.right != null) children.add(e.right);
+            }
+            result.add(line);
+            cur = children;
+        }
+        return result;
+    }
 }
