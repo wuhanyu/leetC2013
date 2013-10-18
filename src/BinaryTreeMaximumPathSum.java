18330a0fec6a1f59448c00903b78d6102eb068db
Simon Wu <wuhanyu@gmail.com>
2013-10-18 19:09:31 +0800

    Update BinaryTreeMaximumPathSum.java

--- src/BinaryTreeMaximumPathSum.java
+++ src/BinaryTreeMaximumPathSum.java
@@ -1,59 +1,27 @@
-import java.util.ArrayList;
-
 /**
- * 
- */
-
-/**
- * @author Simon@itechs
- * @problem Given a binary tree, find the maximum path sum.
- * 
- *          The path may start and end at any node in the tree.
- * 
- *          For example: Given the below binary tree,
- * 
- *          1 / \ 2 3 Return 6.
- *          
- *          
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
 public class BinaryTreeMaximumPathSum {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-
-	static int max;
-
-	public int maxPathSum(TreeNode root) {
-		if (root == null)
-			return 0;
-		max = root.val;
-		int left = getMP(root.left);
-		int right = getMP(root.right);
-		max = Math.max(max, root.val + left + right);
-		return max;
-	}
-
-	int getMP(TreeNode e) {
-		if (e == null)
-			return 0;
-		max = Math.max(max, e.val);
-		int left = getMP(e.left);
-		int right = getMP(e.right);
-		max = Math.max(max, e.val + left + right);
-		return Math.max(0, Math.max(left, right) + e.val);
-	}
-}
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode(int x) {
-		val = x;
-	}
+    static int max;
+    public int maxPathSum(TreeNode root) {
+        max = Integer.MIN_VALUE;
+        maxPathSum2(root);
+        return max;
+    }
+    
+    public int maxPathSum2(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxPathSum2(root.left);
+        int right = maxPathSum2(root.right);
+        if (left < 0) left = 0;
+        if (right < 0) right = 0;
+        if (max < root.val + left + right) max = root.val + left + right;
+        return Math.max(left, right) + root.val;
+    }
 }
