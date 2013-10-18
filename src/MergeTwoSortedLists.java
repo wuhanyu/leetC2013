3353c8b1629777884af2155860b3b5007cc17a9f
Simon Wu <wuhanyu@gmail.com>
2013-10-18 17:31:01 +0800

    Update MergeTwoSortedLists.java

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -1,16 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-        ListNode head;
-        if (l1.val > l2.val) {
-            head = l2;
-            l2 = l2.next;
-        } else {
-            head = l1;
-            l1 = l1.next;
+        ListNode head = null;
+        ListNode p = null;
+        while (l1 != null || l2 != null) {
+            int n1 = (l1 == null ? Integer.MAX_VALUE : l1.val);
+            int n2 = (l2 == null ? Integer.MAX_VALUE : l2.val);
+            if (n1 < n2) {
+                if (head == null) {
+                    head = l1;
+                    p = l1;
+                    l1 = l1.next;
+                } else {
+                    p.next = l1;
+                    p = p.next;
+                    l1 = l1.next;
+                }
+            } else {
+                if (head == null) {
+                    head = l2;
+                    p = l2;
+                    l2 = l2.next;
+                } else {
+                    p.next = l2;
+                    p = p.next;
+                    l2 = l2.next;
+                }
+            }
         }
-        head.next = mergeTwoLists(l1, l2);
+        
         return head;
     }
+    
 }
