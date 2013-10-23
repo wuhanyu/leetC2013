90ffb18964ae5f32e3d85301603a539f2d8918ad
Simon Wu <wuhanyu@gmail.com>
2013-10-23 20:42:50 +0800

    Update BinaryTreeInorderTraversal.java

--- src/BinaryTreeInorderTraversal.java
+++ src/BinaryTreeInorderTraversal.java
@@ -1,56 +1,35 @@
-import java.util.ArrayList;
-import java.util.Stack;
-
 /**
- * 
- */
-
-/**
- * @author Simon@itechs
- * @see http://leetcode.com/onlinejudge#question_93
- * @problem Given a binary tree, return the inorder traversal of its nodes'
- *          values.
- * 
- *          For example: Given binary tree {1,#,2,3},
- * 
- *          1 \ 2 / 3 return [1,3,2].
- * 
- *          Note: Recursive solution is trivial, could you do it iteratively?
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
 public class BinaryTreeInorderTraversal {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public ArrayList<Integer> inorderTraversal(TreeNode root) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		if (root == null)
-			return result;
-		Stack<TreeNode> s = new Stack<TreeNode>();
-		s.push(root);
-		while (root.left != null) {
-			root = root.left;
-			s.push(root);
-		}
-		while (!s.isEmpty()) {
-			TreeNode e = s.pop();
-			result.add(e.val);
-			if (e.right != null) {
-				e = e.right;
-				s.push(e);
-				while (e.left != null) {
-					e = e.left;
-					s.push(e);
-				}
-			}
-
-		}
-		return result;
-	}
-
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (root == null) return result;
+        TreeNode cur = root;
+        TreeNode pre = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                result.add(cur.val);
+                cur = cur.right;
+            } else {
+                pre = cur.left;
+                while (pre.right != null && pre.right != cur) pre = pre.right;
+                if (pre.right == cur) {
+                    result.add(cur.val);
+                    pre.right = null;
+                    cur = cur.right;
+                } else {
+                    pre.right = cur;
+                    cur = cur.left;
+                }
+            }
+        }
+        return result;
+    }
 }
