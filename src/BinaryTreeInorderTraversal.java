7deb4af5b8569f129b83a6831c3e321ef107bc82
Simon Wu <wuhanyu@gmail.com>
2013-10-23 20:46:54 +0800

    Update BinaryTreeInorderTraversal.java

--- src/BinaryTreeInorderTraversal.java
+++ src/BinaryTreeInorderTraversal.java
@@ -7,29 +7,30 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class BinaryTreeInorderTraversal {
+public class BinaryTreeInorderTraversal{
     public ArrayList<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         if (root == null) return result;
         TreeNode cur = root;
         TreeNode pre = null;
         while (cur != null) {
-            if (cur.left == null) {
+            if (cur.right == null) {
                 result.add(cur.val);
-                cur = cur.right;
+                cur = cur.left;
             } else {
-                pre = cur.left;
-                while (pre.right != null && pre.right != cur) pre = pre.right;
-                if (pre.right == cur) {
+                pre = cur.right;
+                while (pre.left != null && pre.left != cur) pre = pre.left;
+                if (pre.left == cur) {
                     result.add(cur.val);
-                    pre.right = null;
-                    cur = cur.right;
-                } else {
-                    pre.right = cur;
+                    pre.left = null;
                     cur = cur.left;
+                } else {
+                    pre.left = cur;
+                    cur = cur.right;
                 }
             }
         }
+        Collections.reverse(result);
         return result;
     }
 }
